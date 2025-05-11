7360fe523a5d31d1f025c298c46b40931c67c8b3
AndresdanielMQ <andresdanielmq@gmail.com>
2025-05-11 16:08:24 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 function openEnvelope() {
   const envelope = document.getElementById('envelope');
   const mensaje = document.getElementById('mensajeAmor');
+  document.getElementById('envelope').classList.add('open');
 
   envelope.classList.add('open');
+
+  // Reproducir música
+  const musica = document.getElementById('audioBackground');
+  musica.play();
 
   // Mostrar el mensaje con un pequeño retardo
   setTimeout(() => {
