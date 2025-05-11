a25fd2bd048431314909e178221ab701278dda07
AndresdanielMQ <andresdanielmq@gmail.com>
2025-05-11 14:52:13 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 function openEnvelope() {
-  document.getElementById('envelope').classList.add('open');
+  const envelope = document.getElementById('envelope');
+  const mensaje = document.getElementById('mensajeAmor');
+
+  envelope.classList.add('open');
+
+  // Mostrar el mensaje con un pequeño retardo
+  setTimeout(() => {
+    mensaje.style.opacity = '1';
+  }, 1000); // Aparece 1 segundo después del clic
 }
